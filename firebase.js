999abd2f75792d934a9fd58efed0dc1b65804738
G.HARIHARAN <77808363+HariharanG26@users.noreply.github.com>
2024-04-26 18:50:48 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from 'firebase/storage';
 import { getFirestore } from 'firebase/firestore/lite';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyApPnPmgt-Mnl4ZfwULWb9gCHfs8s0b0iA",
-  authDomain: "portfolio-2104.firebaseapp.com",
-  projectId: "portfolio-2104",
-  storageBucket: "portfolio-2104.appspot.com",
-  messagingSenderId: "280746243751",
-  appId: "1:280746243751:web:0e0beb432bf79acb5e8d82",
-  measurementId: "G-K4TXX067K5"
+  apiKey: "yourapikey",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 
